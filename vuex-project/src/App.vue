d45b6517060c0405b68b89388401b438bb533dbf
nchen0 <nicky.chen0@gmail.com>
2019-01-18 16:01:39 -0500

    vuex, getters, mutations

---
+++ vuex-project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <h1>Vue App</h1>
+      <product-list-one></product-list-one>
+      <product-list-two v-bind:products="products"></product-list-two>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
+
+export default {
+  name: "app",
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
